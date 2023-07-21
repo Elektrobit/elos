587e3d095615ba9c56df92b6313fd2ee9318f27f
Andreas Schickedanz <andreas.schickedanz@emlix.com>
2023-07-21 14:19:01 +0200

    utest: Update unit tests
    Signed-off-by: Andreas Schickedanz <andreas.schickedanz@emlix.com>

--- elos/test/utest/libelos/elosConnectTcpip/case_exterr_inet_aton.c
+++ elos/test/utest/libelos/elosConnectTcpip/case_exterr_inet_aton.c
@@ -22,7 +22,7 @@ void elosTestElosConnectTcpipExtErrInetAton(UNUSED void **state) {
     MOCK_FUNC_AFTER_CALL(inet_aton, 0);
     expect_value(__wrap_inet_aton, cp, MOCK_IP_ADDR);
     expect_any(__wrap_inet_aton, inp);
-    will_return(__wrap_inet_aton, -1);
+    will_return(__wrap_inet_aton, 0);
 
     result = elosConnectTcpip(MOCK_IP_ADDR, MOCK_PORT, &newSession);
     assert_int_equal(result, SAFU_RESULT_FAILED);
