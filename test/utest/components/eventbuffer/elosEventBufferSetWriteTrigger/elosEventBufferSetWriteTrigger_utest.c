afa1c4b11bcd0c03a9096eda70db4cc1ac9a3720
Christian Steiger <cs@emlix.com>
2023-07-28 08:23:00 +0200

    elos/eventBuffer: Add unit test for function *SetWriteTrigger

---
+++ test/utest/components/eventbuffer/elosEventBufferSetWriteTrigger/elosEventBufferSetWriteTrigger_utest.c
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+#include "elosEventBufferSetWriteTrigger_utest.h"
+
+TEST_SUITE_FUNC_PROTOTYPES(_testSuite)
+
+int main() {
+    const struct CMUnitTest tests[] = {
+        TEST_CASE(elosTestElosEventBufferSetWriteTriggerErrParam),
+        TEST_CASE(elosTestElosEventBufferSetWriteTriggerSuccess),
+    };
+
+    return RUN_TEST_SUITE(tests, _testSuite);
+}
+
+static int _testSuiteSetup(void **state) {
+    elosUnitTestState_t *test;
+
+    test = calloc(1, sizeof(elosUnitTestState_t));
+    assert_non_null(test);
+
+    *state = test;
+
+    return 0;
+}
+
+static int _testSuiteTeardown(void **state) {
+    free(*state);
+    *state = NULL;
+
+    return 0;
+}
