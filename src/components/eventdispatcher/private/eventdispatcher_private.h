646b031a8a13c3a959073816dc33c8cdae9f1ac4
Christian Steiger <cs@emlix.com>
2023-07-28 08:29:12 +0200

    elos/eventDispatcher: Rename private header file

---
+++ src/components/eventdispatcher/private/eventdispatcher_private.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+#pragma once
+
+#include <safu/result.h>
+
+#include "elos/eventdispatcher/defines.h"
+#include "elos/eventdispatcher/types.h"
+
+void *elosEventDispatcherWorker(void *data);
--- src/components/eventdispatcher/private/eventdispatcher_worker.h
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: MIT
-#pragma once
-
-void *elosEventDispatcherWorker(void *data);
