da25295709c3d1691fc37dfe840f70a039fdb6eb
Christian Steiger <cs@emlix.com>
2023-08-11 21:04:40 +0200

    elos/clientmanager: Cleanup code of function Initialize
    Shouldn't change any current behaviour but adapts the code base
    to our current code style as well as organizing things a little better.

--- elos/src/components/clientmanager/private/clientmanager.c
+++ elos/src/components/clientmanager/private/clientmanager.c
@@ -3,9 +3,9 @@
 #include "elos/clientmanager/clientmanager.h"
 
 #include <arpa/inet.h>
+#include <errno.h>
 #include <safu/log.h>
 #include <samconf/samconf.h>
-#include <string.h>
 
 #include "clientmanager_private.h"
 #include "elos/clientmanager/clientauthorization.h"
@@ -13,140 +13,142 @@
 #include "elos/clientmanager/clientblacklist.h"
 #include "elos/config/config.h"
 
-static int _initContext(elosClientManager_t *ctx, elosClientManagerParam_t *parameters) {
-    int retval = -1;
-    memset(ctx, 0, sizeof(elosClientManager_t));
+static safuResultE_t _initializeSharedData(elosClientManager_t *clientmanager, elosClientManagerParam_t *param) {
+    safuResultE_t result = SAFU_RESULT_FAILED;
+    elosClientManagerSharedData_t *sharedData = &clientmanager->sharedData;
+    int retVal;
 
-    samconfConfig_t *config = parameters->config;
-    ctx->sharedData.logAggregator = parameters->logAggregator;
-    ctx->sharedData.eventProcessor = parameters->eventProcessor;
-    ctx->sharedData.eventDispatcher = parameters->eventDispatcher;
-    ctx->addr.sin_family = AF_INET;
-    ctx->addr.sin_port = htons(elosConfigGetElosdPort(config));
-
-    int convertInterface = inet_pton(AF_INET, elosConfigGetElosdInterface(config), &(ctx->addr.sin_addr));
-    if (convertInterface < 1) {
-        safuLogErrErrno("inet_pton failed");
+    retVal = sem_init(&sharedData->connectionSemaphore, 0, CLIENT_MANAGER_MAX_CONNECTIONS);
+    if (retVal != 0) {
+        safuLogErrErrnoValue("sem_init failed!", retVal);
     } else {
-        retval = 0;
+        sharedData->logAggregator = param->logAggregator;
+        sharedData->eventProcessor = param->eventProcessor;
+        sharedData->eventDispatcher = param->eventDispatcher;
+        result = SAFU_RESULT_OK;
     }
 
-    return retval;
+    return result;
 }
 
-static int _initConnections(elosClientManager_t *ctx, elosClientManagerParam_t *parameters) {
-    safuResultE_t status = SAFU_RESULT_OK;
-    int retval = -1, contextLockInit = -1, semLockInit = -1, connectionLockInit = -1;
+static safuResultE_t _initializeListener(elosClientManager_t *clientmanager, samconfConfig_t const *config) {
+    safuResultE_t result = SAFU_RESULT_FAILED;
+    struct sockaddr_in *addr = &clientmanager->addr;
+    char const *interface = elosConfigGetElosdInterface(config);
+    int const port = elosConfigGetElosdPort(config);
+    int retVal;
 
-    contextLockInit = pthread_mutex_init(&(ctx->lock), NULL);
-    if (contextLockInit < 0) {
-        safuLogErrErrno("pthread_mutex_init failed");
+    addr->sin_family = AF_INET;
+    addr->sin_port = htons(port);
+
+    retVal = inet_pton(AF_INET, interface, &addr->sin_addr);
+    if (retVal != 1) {
+        safuLogErrErrnoValue("inet_pton failed", retVal);
+    } else {
+        clientmanager->fd = socket(addr->sin_family, SOCK_STREAM, 0);
+        if (clientmanager->fd == -1) {
+            safuLogErrErrnoValue("socket failed", clientmanager->fd);
+        } else {
+            retVal = setsockopt(clientmanager->fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
+            if (retVal != 0) {
+                safuLogErrErrnoValue("setsocketopt SO_REUSEADDR failed", retVal);
+            } else {
+                retVal = bind(clientmanager->fd, (struct sockaddr *)addr, sizeof(struct sockaddr_in));
+                if (retVal != 0) {
+                    safuLogErrErrnoValue("bind failed", retVal);
+                } else {
+                    // TODO: Move into Start()
+                    retVal = listen(clientmanager->fd, CLIENT_MANAGER_LISTEN_QUEUE_LENGTH);
+                    if (retVal != 0) {
+                        safuLogErrErrnoValue("bind failed", retVal);
+                    } else {
+                        result = SAFU_RESULT_OK;
+                    }
+                }
+            }
+        }
     }
 
-    semLockInit = sem_init(&ctx->sharedData.connectionSemaphore, 0, CLIENT_MANAGER_MAX_CONNECTIONS);
-    if (semLockInit < 0) {
-        safuLogErrErrno("sem_init failed!");
+    return result;
+}
+
+static safuResultE_t _initializeConnections(elosClientManager_t *clientmanager, samconfConfig_t const *config) {
+    safuResultE_t result = SAFU_RESULT_OK;
+
+    for (int i = 0; i < CLIENT_MANAGER_MAX_CONNECTIONS; i += 1) {
+        elosClientManagerConnection_t *connection = &clientmanager->connection[i];
+        int retVal;
+
+        retVal = pthread_mutex_init(&connection->lock, NULL);
+        if (retVal != 0) {
+            safuLogErrF("pthread_mutex_init failed on slot:%d; returned:%d / %s", i, retVal, strerror(errno));
+            break;
+        } else {
+            safuResultE_t iterResult;
+
+            connection->sharedData = &clientmanager->sharedData;
+            connection->status = 0;
+            connection->fd = -1;
+
+            // TODO: Does this really have to be initialized 200+ times?! Looks more like a sharedData entry
+            connection->isTrusted = false;
+            iterResult = elosBlacklistInitialize(&connection->blacklist, config);
+            if (iterResult == SAFU_RESULT_FAILED) {
+                safuLogWarn("blacklist initialization failed");
+            }
+        }
     }
 
-    status = elosAuthorizedProcessInitialize(&ctx->clientAuth.authorizedProcessFilters, parameters->config);
+    return result;
+}
 
-    if (status == SAFU_RESULT_FAILED) {
+static safuResultE_t _initializeAuthorization(elosClientManager_t *clientmanager, samconfConfig_t const *config) {
+    safuResultE_t result = SAFU_RESULT_FAILED;
+
+    result = elosClientAuthorizationInitialize(&clientmanager->clientAuth);
+    if (result != SAFU_RESULT_OK) {
+        safuLogWarn("elosClientAuthorizationInitialize failed");
+    }
+
+    result = elosAuthorizedProcessInitialize(&clientmanager->clientAuth.authorizedProcessFilters, config);
+    if (result != SAFU_RESULT_OK) {
         safuLogWarn("authorized process filter initialization failed");
     } else {
         safuLogDebug("authorized process filter initialization succeeded");
     }
 
-    elosClientManagerConnection_t *conn = NULL;
-    for (int i = 0; i < CLIENT_MANAGER_MAX_CONNECTIONS; i += 1) {
-        conn = &ctx->connection[i];
-        connectionLockInit = pthread_mutex_init(&conn->lock, NULL);
-        if (connectionLockInit < 0) {
-            safuLogErrF("pthread_mutex_init 'slot:%d'", i);
-            break;
-        }
-
-        conn->isTrusted = false;
-        status = elosBlacklistInitialize(&conn->blacklist, parameters->config);
-
-        if (status == SAFU_RESULT_FAILED) {
-            safuLogWarn("blacklist initialization failed");
-        }
-
-        conn->sharedData = &ctx->sharedData;
-        conn->status = 0;
-        conn->fd = -1;
-    }
-
-    if (contextLockInit >= 0 && semLockInit >= 0 && connectionLockInit >= 0) {
-        retval = 0;
-    }
-
-    return retval;
-}
-
-static int _listenOnNewSocket(elosClientManager_t *ctx) {
-    int retval = -1, sockOpt = -1, bindSock = -1, listenSock = -1;
-
-    ctx->fd = socket(ctx->addr.sin_family, SOCK_STREAM, 0);
-    if (ctx->fd < 0) {
-        safuLogErrErrno("socket failed");
-    } else {
-        sockOpt = setsockopt(ctx->fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
-    }
-
-    if (sockOpt != 0) {
-        safuLogErrErrno("setsocketopt SO_REUSEADDR failed");
-    } else {
-        bindSock = bind(ctx->fd, (struct sockaddr *)&(ctx->addr), sizeof(ctx->addr));
-    }
-
-    if (bindSock != 0) {
-        safuLogErrErrno("bind failed");
-    } else {
-        listenSock = listen(ctx->fd, CLIENT_MANAGER_LISTEN_QUEUE_LENGTH);
-    }
-
-    if (listenSock < 0) {
-        safuLogErrErrno("listen failed");
-    } else {
-        retval = 0;
-    }
-
-    return retval;
+    return result;
 }
 
 safuResultE_t elosClientManagerInitialize(elosClientManager_t *clientmanager, elosClientManagerParam_t *param) {
     safuResultE_t result = SAFU_RESULT_FAILED;
 
+    // TODO: Detect multiple uses of elosClientManagerStart properly?
+
     if ((clientmanager == NULL) || (param == NULL)) {
-        safuLogErr("Invalid parameter");
+        safuLogErr("Invalid parameter NULL");
+    } else if ((param->config == NULL) || (param->eventDispatcher == NULL) || (param->eventProcessor == NULL) ||
+               (param->logAggregator == NULL)) {
+        safuLogErr("Invalid value NULL in parameter struct");
     } else {
-        int initContext = -1, initThreadLocks = -1, listenOnNewSocket = -1;
+        int retVal;
 
         memset(clientmanager, 0, sizeof(elosClientManager_t));
 
-        // TODO: Detect multiple uses of elosClientManagerStart properly?
-
-        if (clientmanager == NULL || param == NULL) {
-            safuLogErr("Called elosClientManagerStart with parameter being NULL");
+        retVal = pthread_mutex_init(&clientmanager->lock, NULL);
+        if (retVal != 0) {
+            safuLogErrErrnoValue("pthread_mutex_init failed", retVal);
         } else {
-            initContext = _initContext(clientmanager, param);
-        }
-
-        if (initContext == 0) {
-            initThreadLocks = _initConnections(clientmanager, param);
-        }
-
-        if (initThreadLocks == 0) {
-            elosClientAuthorizationInitialize(&clientmanager->clientAuth);
-        }
-
-        if (initThreadLocks == 0) {
-            listenOnNewSocket = _listenOnNewSocket(clientmanager);
-        }
-
-        if (listenOnNewSocket == 0) {
-            result = SAFU_RESULT_OK;
+            result = _initializeSharedData(clientmanager, param);
+            if (result == SAFU_RESULT_OK) {
+                result = _initializeListener(clientmanager, param->config);
+                if (result == SAFU_RESULT_OK) {
+                    result = _initializeConnections(clientmanager, param->config);
+                    if (result == SAFU_RESULT_OK) {
+                        _initializeAuthorization(clientmanager, param->config);
+                    }
+                }
+            }
         }
     }
 
