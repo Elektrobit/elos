ef0a8681871487aa764a9e4e21f80edb69d201d1
Christian Steiger <cs@emlix.com>
2023-08-23 16:51:53 +0200

    elos/clientmanager: Add documentation to the header files

--- elos/src/components/clientmanager/interface/elos/clientmanager/clientconnection_types.h
+++ elos/src/components/clientmanager/interface/elos/clientmanager/clientconnection_types.h
@@ -15,11 +15,28 @@
 typedef safuVec_t elosEventFilterNodeIdVector_t;
 typedef safuVec_t elosEventQueueIdVector_t;
 
+/*******************************************************************
+ * Data structures used exclusively by the worker thread
+ *
+ * Members:
+ *   eventFilterNodeIdVector: FilterNodes used by the connection
+ *   eventQueueIdVector: EventQueues used by the connection
+ ******************************************************************/
 typedef struct elosClientConnectionData {
     elosEventFilterNodeIdVector_t eventFilterNodeIdVector;
     elosEventQueueIdVector_t eventQueueIdVector;
 } elosClientConnectionData_t;
 
+/*******************************************************************
+ * Data structures shared between all connections of a ClientManager
+ *
+ * Members:
+ *   logAggregator: Used for persistent logging of Events
+ *   connectionSemaphore: Used for waiting until a connection is available
+ *   eventDispatcher: Used for registering the EventBuffers of each Connection
+ *   eventProcessor: Used for FilterNode/EventQueue handling
+ *   config: Static configuration variables
+ ******************************************************************/
 typedef struct elosClientConnectionSharedData {
     elosLogAggregator_t *logAggregator;
     sem_t connectionSemaphore;
@@ -28,6 +45,22 @@ typedef struct elosClientConnectionSharedData {
     samconfConfig_t *config;
 } elosClientConnectionSharedData_t;
 
+/*******************************************************************
+ * Data structure of a ClientConnection
+ *
+ * Members:
+ *   flags: State bits of the component (e.g. initialized, active, e.t.c.)
+ *   fd: The socket used for communication
+ *   syncFd: eventfd used for synchronization with the worker thread
+ *   triggerFd: eventfd used for waking the worker thread (e.g. for a controlled shutdown)
+ *   addr: Address information of the currently used socketFd
+ *   thread: Worker thread used by pthread_* functions
+ *   sharedData: See struct defintion of elosClientConnectionSharedData_t
+ *   data: worker thread local data
+ *   isTrusted: 'true' if the connection is trusted
+ *   blacklist: blacklist filters
+ *   eventBuffer: Buffer for incoming Events.
+ ******************************************************************/
 typedef struct elosClientConnection {
     safuFlags_t flags;
     int fd;
@@ -42,6 +75,12 @@ typedef struct elosClientConnection {
     elosEventBuffer_t eventBuffer;
 } elosClientConnection_t;
 
+/*******************************************************************
+ * Initialization parameters for a new ClientConnection
+ *
+ * Members:
+ *   sharedData: See definition of elosClientConnectionSharedData_t
+ ******************************************************************/
 typedef struct elosClientConnectionParam {
     elosClientConnectionSharedData_t *sharedData;
 } elosClientConnectionParam_t;
--- elos/src/components/clientmanager/public/elos/clientmanager/clientmanager.h
+++ elos/src/components/clientmanager/public/elos/clientmanager/clientmanager.h
@@ -3,7 +3,51 @@
 
 #include "elos/clientmanager/types.h"
 
-safuResultE_t elosClientManagerInitialize(elosClientManager_t *clientmanager, elosClientManagerParam_t *param);
-safuResultE_t elosClientManagerDeleteMembers(elosClientManager_t *clientmanager);
-safuResultE_t elosClientManagerStart(elosClientManager_t *clientmanager);
-safuResultE_t elosClientManagerStop(elosClientManager_t *clientmanager);
+/*******************************************************************
+ * Initializes a new ClientManager.
+ *
+ * Parameters:
+ *      clientManager: ClientManager structure that will be initialized
+ *      param: Parameters for initialization. Also see clientmanager_types.h
+ * Returns:
+ *      - SAFU_RESULT_FAILED on error
+ *      - SAFU_RESULT_OK on success
+ ******************************************************************/
+safuResultE_t elosClientManagerInitialize(elosClientManager_t *clientManager, elosClientManagerParam_t *param);
+
+/*******************************************************************
+ * Frees member resources used by the given ClientManager data structure.
+ * Will also automatically stop an active worker thread before
+ * freeing up the used resources.
+ *
+ * Parameters:
+ *      clientManager: ClientManager whose members shall be freed
+ * Returns:
+ *      - SAFU_RESULT_FAILED on error
+ *      - SAFU_RESULT_OK on success
+ ******************************************************************/
+safuResultE_t elosClientManagerDeleteMembers(elosClientManager_t *clientManager);
+
+/*******************************************************************
+ * Creates a worker thread waiting for incoming connections.
+ * Each ClientManager can only handle one worker thread,
+ * calling Start a second time will result in an error.
+ *
+ * Parameters:
+ *      clientManager: ClientManager structure to started
+ * Returns:
+ *      - SAFU_RESULT_FAILED on error
+ *      - SAFU_RESULT_OK on success
+ ******************************************************************/
+safuResultE_t elosClientManagerStart(elosClientManager_t *clientManager);
+
+/*******************************************************************
+ * Stops a currently active worker thread.
+ *
+ * Parameters:
+ *      clientManager: ClientManager structure to stopped
+ * Returns:
+ *      - SAFU_RESULT_FAILED on error
+ *      - SAFU_RESULT_OK on success
+ ******************************************************************/
+safuResultE_t elosClientManagerStop(elosClientManager_t *clientManager);
