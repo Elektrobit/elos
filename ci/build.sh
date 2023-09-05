5d452be4bd4c0fed1a4ed66f332877f45d717f45
Friedrich Schwedler <fschwedler@emlix.com>
2023-09-05 15:16:42 +0200

    add dependency fechting and installing
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

--- ci/build.sh
+++ ci/build.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
-set -e -u -o pipefail
+set -e -u
 
-CMD_PATH=$(realpath $(dirname $0))
+CMD_PATH=$(realpath "$(dirname "$0")")
 BASE_DIR=${CMD_PATH%/*}
-REPO_ROOT_DIR="$BASE_DIR/.."
 CMAKE_PARAM=${CMAKE_PARAM:-""}
 NINJA_PARAM=${NINJA_PARAM:-"-j$(nproc)"}
+SOURCES_URI=${SOURCES_URI:-https://github.com/emlix/}
 
-export LC_ALL=C
+CMOCKA_EXTENSIONS_REPO_NAME=${CMOCKA_EXTENSIONS_REPO_NAME:-cmocka-extensions.git}
+CMOCKA_EXTENSIONS_REPO_PATH=${CMOCKA_EXTENSIONS_REPO_PATH:-${SOURCES_URI}/${CMOCKA_EXTENSIONS_REPO_NAME}}
+CMOCKA_EXTENSIONS_REPO_REF=${CMOCKA_EXTENSIONS_REPO_REF:-integration}
+
+CMOCKA_MOCKS_REPO_NAME=${CMOCKA_MOCKS_REPO_NAME:-cmocka-mocks.git}
+CMOCKA_MOCKS_REPO_PATH=${CMOCKA_MOCKS_REPO_PATH:-${SOURCES_URI}/${CMOCKA_MOCKS_REPO_NAME}}
+CMOCKA_MOCKS_REPO_REF=${CMOCKA_MOCKS_REPO_REF:-integration}
+
+SAFU_REPO_NAME=${SAFU_REPO_NAME:-safu.git}
+SAFU_REPO_PATH=${SAFU_REPO_PATH:-${SOURCES_URI}/${SAFU_REPO_NAME}}
+SAFU_REPO_REF=${SAFU_REPO_REF:-integration}
+
+SAMCONF_REPO_NAME=${SAMCONF_REPO_NAME:-samconf.git}
+SAMCONF_REPO_PATH=${SAMCONF_REPO_PATH:-${SOURCES_URI}/${SAMCONF_REPO_NAME}}
+SAMCONF_REPO_REF=${SAMCONF_REPO_REF:-integration}
 
 PARAM=""
 OPTION_CI=0
 OPTION_CLEAN=0
 OPTION_VERBOSE=0
 OPTION_PACKAGE=0
-for element in $@; do
+for element in "$@"; do
     case $element in
         --ci)          OPTION_CI=1 ;;
         --clean|-c)    OPTION_CLEAN=1 ;;
@@ -43,22 +57,32 @@ BUILD_TYPE="${1:-Debug}"
 
 if [ $OPTION_PACKAGE -eq 1 ]; then
     CMAKE_PARAM="${CMAKE_PARAM} -D PACKAGING=true"
-    BUILD_TYPE="Release"
+    BUILD_TYPE=Release
     OPTION_CLEAN=1
 fi
 
-BUILD_DIR="$BASE_DIR/build/$BUILD_TYPE"
-RESULT_DIR="$BUILD_DIR/result"
-DIST_DIR="$BUILD_DIR/dist"
-CMAKE_BUILD_DIR="$BUILD_DIR/cmake"
-export LOCAL_INSTALL_DIR=${LOCAL_INSTALL_DIR:-"$DIST_DIR"}
+CMAKE_PARAM="${CMAKE_PARAM} -D CMOCKA_EXTENSIONS_URI=${CMOCKA_EXTENSIONS_REPO_PATH} \
+                            -D CMOCKA_EXTENSIONS_REF=${CMOCKA_EXTENSIONS_REPO_REF}"
+CMAKE_PARAM="${CMAKE_PARAM} -D CMOCKA_MOCKS_URI=${CMOCKA_MOCKS_REPO_PATH} \
+                            -D CMOCKA_MOCKS_REF=${CMOCKA_MOCKS_REPO_REF}"
+CMAKE_PARAM="${CMAKE_PARAM} -D SAFU_URI=${SAFU_REPO_PATH} \
+                            -D SAFU_REF=${SAFU_REPO_REF}"
+CMAKE_PARAM="${CMAKE_PARAM} -D SAMCONF_URI=${SAMCONF_REPO_PATH} \
+                            -D SAMCONF_REF=${SAMCONF_REPO_REF}"
+
+BUILD_DIR=$BASE_DIR/build/$BUILD_TYPE
+RESULT_DIR=$BUILD_DIR/result
+DIST_DIR=$BUILD_DIR/dist
+CMAKE_BUILD_DIR=$BUILD_DIR/cmake
+export LOCAL_INSTALL_DIR=${LOCAL_INSTALL_DIR:-$DIST_DIR}
+CMAKE_PARAM="${CMAKE_PARAM} -D INSTALL_DIR=${LOCAL_INSTALL_DIR}"
 
 DEP_BUILD_PARAM=""
 if [ $OPTION_CLEAN -eq 1 ]; then
     DEP_BUILD_PARAM="$DEP_BUILD_PARAM -c"
-    if [ -e $BUILD_DIR ]; then
+    if [ -e "$BUILD_DIR" ]; then
         echo "Removing $BUILD_DIR ..."
-        rm -rf $BUILD_DIR
+        rm -rf "$BUILD_DIR"
     fi
 fi
 if [ $OPTION_VERBOSE -eq 1 ]; then
@@ -66,24 +90,17 @@ if [ $OPTION_VERBOSE -eq 1 ]; then
     NINJA_PARAM="$NINJA_PARAM -v"
 fi
 
-export mock_safu_DIR="$LOCAL_INSTALL_DIR/usr/local/lib/cmake/mock_safu"
-DEPENDENCIES="safu samconf cmocka_mocks cmocka_extensions"
-for dependency in $DEPENDENCIES; do
-    declare -x ${dependency}_DIR="$LOCAL_INSTALL_DIR/usr/local/lib/cmake/${dependency}"
-    $REPO_ROOT_DIR/${dependency}/ci/build.sh $BUILD_TYPE $DEP_BUILD_PARAM
-done
-
-echo -e "\n#### Building $(basename $BASE_DIR) ($BUILD_TYPE) ####"
-mkdir -p $RESULT_DIR $DIST_DIR
-if [ ! -e $CMAKE_BUILD_DIR/build.ninja ]; then
-    cmake -B $CMAKE_BUILD_DIR $BASE_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE -G Ninja $CMAKE_PARAM
+echo -e "\n#### Building $(basename "$BASE_DIR") ($BUILD_TYPE) ####"
+mkdir -p "$RESULT_DIR" "$DIST_DIR"
+if [ ! -e "$CMAKE_BUILD_DIR/build.ninja" ]; then
+    cmake -B "$CMAKE_BUILD_DIR" "$BASE_DIR" "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" -G Ninja $CMAKE_PARAM
 fi
 
 DESTDIR="$LOCAL_INSTALL_DIR" \
-ninja -C $CMAKE_BUILD_DIR $NINJA_PARAM all install 2>&1 | tee $RESULT_DIR/build_log.txt
+ninja -C "$CMAKE_BUILD_DIR" $NINJA_PARAM all install 2>&1 | tee "$RESULT_DIR/build_log.txt"
 
 re=${PIPESTATUS[0]}
 
-$REPO_ROOT_DIR/shared/ci/check_build_log.py $RESULT_DIR/build_log.txt
+"$BASE_DIR/ci/check_build_log.py" "$RESULT_DIR/build_log.txt"
 
 exit $re
--- project.cmake
+++ project.cmake
@@ -87,12 +87,15 @@ endfunction()
 
 
 function(project_add_documentation_target)
+  if (TARGET ${PROJECT_NAME}_doc)
+    return()
+  endif ()
   cmake_parse_arguments(PARAM "" "MARKDOWN;TITLE" "" ${ARGN})
 
   find_program(PLANTUML NAMES plantuml)
   find_program(PANDOC NAMES pandoc)
   add_custom_target(
-    doc
+    ${PROJECT_NAME}_doc
     mkdir -p ${CMAKE_BINARY_DIR}/doc/images &&
     ${PLANTUML} ${CMAKE_SOURCE_DIR}/documentation/images/*.plantuml &&
     ${PANDOC} -s --from gfm --to html --filter pandoc-plantuml -o ${CMAKE_BINARY_DIR}/doc/${PROJECT_NAME}_doc.html --css style.css ${PARAM_MARKDOWN} --metadata title='${PARAM_TITLE}' &&
