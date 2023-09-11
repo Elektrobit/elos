8bd62ceb110f1b358e58c5ea35e045bbf83609bf
Vignesh Jayaraman <vjayaraman@emlix.com>
2023-09-11 17:45:39 +0200

    elos log api : utest for eloslog edited to conform to refactored code

--- elos/test/utest/components/eloslog/elosLogCreateElosEventFromLog/case_success.c
+++ elos/test/utest/components/eloslog/elosLogCreateElosEventFromLog/case_success.c
@@ -18,45 +18,24 @@ int elosLogCreateElosEventFromLogSuccessTeardown(UNUSED void **state) {
 }
 
 void elosLogCreateElosEventFromLogSuccess(UNUSED void **state) {
-    ssize_t fileNameSize = 0;
-    elosLogStatusE_t result = ELOS_LOG_STATUS_ERROR;
-    elosEvent_t *testEvent = NULL;
-    elosLogData_t *testLog = NULL;
+    elosEvent_t testEvent = {0};
     const char *testMessage = "testMessage";
-    char *appName = NULL;
-    char *fileName = NULL;
+
+    pid_t testpid = getpid();
 
     TEST("elosLogCreateElosEventFromLog");
     SHOULD("%s", "creates an elos event from log parameters");
 
-    pid_t testpid = getpid();
+    elosLogCreateElosEventFromLog(ELOS_MSG_CODE_DEBUG_LOG, ELOS_SEVERITY_DEBUG, ELOS_CLASSIFICATION_PROCESS,
+                                  testMessage, &testEvent);
 
-    fileName = safuAllocMem(NULL, PATH_MAX);
-    fileNameSize = readlink("/proc/self/exe", fileName, PATH_MAX);
+    assert_int_not_equal(testEvent.date.tv_sec, 0);
+    assert_int_not_equal(testEvent.date.tv_nsec, 0);
+    assert_int_equal(testEvent.source.pid, testpid);
+    assert_int_equal(testEvent.messageCode, ELOS_MSG_CODE_DEBUG_LOG);
+    assert_int_equal(testEvent.severity, ELOS_SEVERITY_DEBUG);
+    assert_int_equal(testEvent.classification, ELOS_CLASSIFICATION_PROCESS);
+    assert_string_equal(testEvent.payload, testMessage);
 
-    fileName[fileNameSize] = 0;
-    appName = strdup(strrchr(fileName, '/'));
-    fileName = safuAllocMem(fileName, fileNameSize + sizeof('\0'));
-
-    elosLogCreateLogData(ELOS_MSG_CODE_DEBUG_LOG, ELOS_SEVERITY_DEBUG, ELOS_CLASSIFICATION_PROCESS, testMessage,
-                         &testLog);
-    assert_non_null(testLog);
-
-    result = elosLogCreateElosEventFromLog(testLog, &testEvent);
-
-    assert_int_equal(result, ELOS_LOG_STATUS_SUCCESS);
-    assert_int_not_equal(testEvent->date.tv_sec, 0);
-    assert_int_not_equal(testEvent->date.tv_nsec, 0);
-    assert_string_equal(testEvent->source.appName, appName);
-    assert_string_equal(testEvent->source.fileName, fileName);
-    assert_int_equal(testEvent->source.pid, testpid);
-    assert_int_equal(testEvent->messageCode, ELOS_MSG_CODE_DEBUG_LOG);
-    assert_int_equal(testEvent->severity, ELOS_SEVERITY_DEBUG);
-    assert_int_equal(testEvent->classification, ELOS_CLASSIFICATION_PROCESS);
-    assert_string_equal(testEvent->payload, testMessage);
-
-    free(fileName);
-    free(appName);
-    elosLogDeleteLogData(testLog);
-    elosEventDelete(testEvent);
+    elosEventDeleteMembers(&testEvent);
 }
--- elos/test/utest/components/eloslog/elosLogCreateElosEventFromLog/elosLogCreateElosEventFromLog_utest.h
+++ elos/test/utest/components/eloslog/elosLogCreateElosEventFromLog/elosLogCreateElosEventFromLog_utest.h
@@ -5,11 +5,8 @@
 #include <cmocka_extensions/mock_extensions.h>
 
 #include "elos/eloslog/eloslog.h"
-#include "elos/eloslog/types.h"
 #include "elos/event/event.h"
 #include "mock_event.h"
 
 TEST_CASE_FUNC_PROTOTYPES(elosLogCreateElosEventFromLogSuccess)
-TEST_CASE_FUNC_PROTOTYPES(elosLogCreateElosEventFromLogErrParamLogData)
 TEST_CASE_FUNC_PROTOTYPES(elosLogCreateElosEventFromLogErrParamEvent)
-TEST_CASE_FUNC_PROTOTYPES(elosLogCreateElosEventFromLogExtErrEvent)
