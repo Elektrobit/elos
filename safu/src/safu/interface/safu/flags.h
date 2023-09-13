93a87a0c85eccead0399ce2560b08bff6b9e36bd
Vignesh Jayaraman <vjayaraman@emlix.com>
2023-09-13 12:49:30 +0200

    safu : flags for logger initiaize added

--- safu/src/safu/interface/safu/flags.h
+++ safu/src/safu/interface/safu/flags.h
@@ -18,6 +18,12 @@ typedef atomic_uint_least32_t safuFlags_t;
 #define SAFU_FLAG_CUSTOM_START_BIT (1 << 16)
 #define SAFU_FLAG_INITIALIZED_BIT  (1 << 0)
 #define SAFU_FLAG_ERROR_BIT        (1 << 1)
+#define SAFU_FLAG_SHUTDOWN_BIT     (1 << 2)
 
+#define SAFU_FLAG_CLEAR_ALL_BITS(__flag)      atomic_store(__flag, SAFU_FLAG_NONE)
 #define SAFU_FLAG_HAS_INITIALIZED_BIT(__flag) ((atomic_load(__flag) & SAFU_FLAG_INITIALIZED_BIT) != 0)
 #define SAFU_FLAG_HAS_ERROR_BIT(__flag)       ((atomic_load(__flag) & SAFU_FLAG_ERROR_BIT) != 0)
+#define SAFU_FLAG_EXCHANGE_INITIALIZE_BIT(__flag) \
+    ((atomic_fetch_or(__flag, SAFU_FLAG_INITIALIZED_BIT) & (SAFU_FLAG_INITIALIZED_BIT | SAFU_FLAG_SHUTDOWN_BIT)) == 0)
+#define SAFU_FLAG_EXCHANGE_SHUTDOWN_BIT(__flag) \
+    ((atomic_fetch_or(__flag, SAFU_FLAG_SHUTDOWN_BIT) & SAFU_FLAG_INITIALIZED_BIT) == 0)
