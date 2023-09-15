553d3fb1e1fdde81e479ae91defca7cd4c377fdb
Wolfgang Gehrhardt <wolfgang.gehrhardt@emlix.com>
2023-09-15 11:49:16 +0200

    elos: Update to 0.48.6

--- shared/cmake/project.cmake
+++ shared/cmake/project.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-set(ELOS_VERSION 0.48.5)
+set(ELOS_VERSION 0.48.6)
 
 # Attention: Aside from the version, as many things as possible in this file
 #   should be put into functions, as this solves potential issues with commands
