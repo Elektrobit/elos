b229b07bbf39f1eea7a772b6ef0d63c91fc96ef6
Friedrich Schwedler <fschwedler@emlix.com>
2023-10-05 13:33:53 +0200

    add file with common names for build and test scripts
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

---
+++ ci/common_names.sh
@@ -0,0 +1,12 @@
+
+## to include in any script
+# CMD_PATH="$(realpath "$(dirname "$0")")"
+# BASE_DIR="$(realpath "$CMD_PATH/..")" # replace .. by the path to project root
+# . "$BASE_DIR/ci/common_names.sh"
+
+PROJECT="${PROJECT:-${BASE_DIR##*/}}"
+BUILD_TYPE="${BUILD_TYPE:-Debug}"
+BUILD_DIR="${BUILD_DIR:-${BASE_DIR}/build/${BUILD_TYPE}}"
+DIST_DIR="${DIST_DIR:-"${BUILD_DIR}/dist"}"
+RESULT_DIR="${RESULT_DIR:-${BUILD_DIR}/result}"
+
