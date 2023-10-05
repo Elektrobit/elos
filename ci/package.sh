33cc4ef0b46cd33db520713d4722e46e06ff2285
Friedrich Schwedler <fschwedler@emlix.com>
2023-10-05 15:08:41 +0200

    common names for packaging
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

--- ci/package.sh
+++ ci/package.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e -u -o pipefail
 
-CMD_PATH=$(realpath $(dirname $0))
-BASE_DIR=${CMD_PATH%/*}
-
+CMD_PATH="$(realpath "$(dirname "$0")")"
+BASE_DIR="$(realpath "$CMD_PATH/..")"
 BUILD_TYPE="${1:-Release}"
-BUILD_DIR="$BASE_DIR/build/$BUILD_TYPE"
+. "$BASE_DIR/ci/common_names.sh"
 CMAKE_BUILD_DIR="$BUILD_DIR/cmake"
 
-cd $CMAKE_BUILD_DIR
+cd "$CMAKE_BUILD_DIR"
 cpack -G DEB
