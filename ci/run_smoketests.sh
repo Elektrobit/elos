72f0a6f4a7ac04e070921df388f1238f2414d28f
Friedrich Schwedler <fschwedler@emlix.com>
2023-10-05 15:09:49 +0200

    common names to run_smoketests
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

--- ci/run_smoketests.sh
+++ ci/run_smoketests.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -eou pipefail
 
-CMD_PATH=$(cd $(dirname $0) && pwd)
-BASE_DIR=${CMD_PATH%/*}
-
+CMD_PATH="$(realpath "$(dirname "$0")")"
+BASE_DIR="$(realpath "$CMD_PATH/..")"
 BUILD_TYPE="${1:-Debug}"
-BUILD_DIR="$BASE_DIR/build/$BUILD_TYPE"
+. "$BASE_DIR/ci/common_names.sh"
 
-rm -rf $BUILD_DIR/result/smoketest_results
-mkdir -p $BUILD_DIR/result/smoketest_results
+rm -rf $RESULT_DIR/smoketest_results
+mkdir -p $RESULT_DIR/smoketest_results
 
 export ENABLED_TESTS="${2:-""}"
 export DISABLED_TESTS=""
 BASE_DIR=$BASE_DIR BUILD_TYPE=$BUILD_TYPE \
 $BASE_DIR/test/smoketest/smoketest.sh | \
-tee $BUILD_DIR/result/smoketest_results/smoktests.log
+tee "$RESULT_DIR/smoketest_results/smoktests.log"
 
 #tapview > $BUILD_DIR/result/smoketest_results/summary < $BUILD_DIR/result/smoketest_results/log
