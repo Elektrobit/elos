e2918bd3d57952fa38b3e58b5fbac5f3b2d8827e
Christian Steiger <cs@emlix.com>
2023-10-20 15:39:40 +0200

    config: Split headers and add interface target
    Moves the defaults paths into a separate defines.h
    that also gets its own interface target to avoid
    having to link the entire component when we're only
    interested in the samconf path strings.

---
+++ src/components/config/interface/elos/config/defines.h
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+#pragma once
+
+#ifndef ELOS_CONFIG_ROOT
+#define ELOS_CONFIG_ROOT "/root/elos/"
+#endif
+
+#ifndef ELOS_CONFIG_EVENTLOGGING
+#define ELOS_CONFIG_EVENTLOGGING ELOS_CONFIG_ROOT "EventLogging/"
+#endif
+
+#ifndef ELOS_CONFIG_SCANNER
+#define ELOS_CONFIG_SCANNER ELOS_CONFIG_ROOT "Scanner/"
+#endif
+
+#ifndef ELOS_CONFIG_STORAGEBACKEND
+#define ELOS_CONFIG_STORAGEBACKEND ELOS_CONFIG_ROOT "StorageBackend/"
+#endif
