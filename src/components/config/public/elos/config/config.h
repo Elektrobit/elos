cc11922aaf97d86877aaa51d0668633504af0ed2
Vignesh Jayaraman <vjayaraman@emlix.com>
2023-11-13 13:40:36 +0100

    add documentation for connection limit in configuration

--- src/components/config/public/elos/config/config.h
+++ src/components/config/public/elos/config/config.h
@@ -9,6 +9,15 @@
 
 int elosConfigLoad(samconfConfig_t **config);
 int elosConfigGetElosdPort(const samconfConfig_t *config);
+/*******************************************************************
+ * Get the connection limit from config
+ *
+ * Parameters:
+ *      config (samconfConfig_t *): elos configuration struct
+ * Returns:
+ *      - set connection value on success
+ *      - default value(200)  on failure
+ ******************************************************************/
 int elosConfigGetElosdConnectionLimit(const samconfConfig_t *config);
 const char *elosConfigGetElosdInterface(const samconfConfig_t *config);
 const char *elosConfigGetElosdLogFilter(const samconfConfig_t *config);
