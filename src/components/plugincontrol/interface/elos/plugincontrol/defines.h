059de2b313fc37fa34efa91c1ddc4c77e2d59f30
Christian Steiger <cs@emlix.com>
2023-11-29 15:30:15 +0100

    pluginControl: Add basic documentation

--- src/components/plugincontrol/interface/elos/plugincontrol/defines.h
+++ src/components/plugincontrol/interface/elos/plugincontrol/defines.h
@@ -14,10 +14,32 @@
 #define ELOS_PLUGINCONTROL_FLAG_HAS_PLUGINERROR_BIT(__f) \
     ((atomic_load(__f) & ELOS_PLUGINCONTROL_FLAG_PLUGINERROR_BIT) != 0)
 
-// Atomic set bit and test bit. Is 'true' when the given bit went from 0 to 1 during the operation
+/*******************************************************************
+ * Atomic set bit and test bit.
+ * Returns 'true' when the given bit went from 0 to 1 during the operation.
+ *
+ * Parameters:
+ *      __f : Pointer to a safuFlags_t field
+ *      __b : The bit to be set and tested.
+ *
+ * Returns:
+ *      - `true` when the given bit went from 0 to 1 during the operation.
+ *      - `false` when the given bit did not change
+ ******************************************************************/
 #define ELOS_PLUGINCONTROL_FLAG_PUSH_BIT(__f, __b) ((atomic_fetch_or(__f, (__b)) & (__b)) == 0)
 
-// Atomic clear bit and test bit. Is 'true' when the given bit went from 1 to 0 during the operation
+/*******************************************************************
+ * Atomic clear bit and test bit.
+ * Is 'true' when the given bit went from 1 to 0 during the operation
+ *
+ * Parameters:
+ *      __f : Pointer to a safuFlags_t field
+ *      __b : The bit to be cleared and tested.
+ *
+ * Returns:
+ *      - `true` when the given bit went from 1 to 0 during the operation
+ *      - `false` when the given bit did not change
+ ******************************************************************/
 #define ELOS_PLUGINCONTROL_FLAG_PULL_BIT(__f, __b) ((atomic_fetch_and(__f, ~(__b)) & (__b)) != 0)
 
 #define ELOS_PLUGINCONTROL_FLAG_PUSH_LOADED_BIT(__f) \
