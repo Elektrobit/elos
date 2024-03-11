b5f17fdf09ffd1c1fd724a381f1550b6843c3463
Vignesh Jayaraman <vjayaraman@emlix.com>
2024-03-11 17:23:51 +0100

    edit elosPluginControlLoad utest
    add mock for pthread_setname_np

--- test/utest/components/plugincontrol/elosPluginControlLoad/elosPluginControlLoad_utest.h
+++ test/utest/components/plugincontrol/elosPluginControlLoad/elosPluginControlLoad_utest.h
@@ -2,6 +2,13 @@
 #pragma once
 
 #include <cmocka_extensions/cmocka_extensions.h>
+#include <cmocka_extensions/mock_extensions.h>
+#include <json-c/json.h>
+#include <json-c/json_tokener.h>
+#include <safu/common.h>
+#include <safu/log.h>
+#include <samconf/mock_samconf.h>
+#include <samconf/samconf.h>
 
 #include "elos/plugincontrol/plugincontrol.h"
 
@@ -12,6 +19,8 @@ typedef struct elosPluginTestData {
 typedef struct elosUnitTestState {
     elosPluginControl_t pluginControl;
     elosPluginTestData_t data;
+    samconfConfig_t *mockRootConfig;
+    samconfConfig_t const *mockPluginConfig;
 } elosUnitTestState_t;
 
 #define _CUSTOM_LOAD_BIT   (1 << 0)
@@ -23,6 +32,10 @@ safuResultE_t elosCustomLoad(elosPlugin_t *plugin);
 safuResultE_t elosCustomStart(elosPlugin_t *plugin);
 safuResultE_t elosCustomStop(elosPlugin_t *plugin);
 safuResultE_t elosCustomUnload(elosPlugin_t *plugin);
+
+samconfConfigStatusE_t elosUtilCreateMockConfig(json_object *jobj, bool isSigned, samconfConfig_t *config);
+samconfConfigStatusE_t elosGetMockConfig(samconfConfig_t *config);
+int elosMockConfigCleanup(samconfConfig_t *config);
 
 extern elosPluginConfig_t const elosPluginConfig;
 
