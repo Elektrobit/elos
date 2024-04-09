3227bacb0082bad1f94ec922e843fd427197efea
Friedrich Schwedler <fschwedler@emlix.com>
2024-04-09 19:24:28 +0200

    add unit tests for ScannerManagerStart
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

---
+++ test/utest/components/scannermanager/elosScannerManagerStart/elosScannerManagerStart_utest.h
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+#pragma once
+
+#include <cmocka_extensions/cmocka_extensions.h>
+#include <cmocka_extensions/mock_extensions.h>
+#include <json-c/json.h>
+#include <json-c/json_tokener.h>
+#include <safu/mock_safu.h>
+#include <samconf/mock_samconf.h>
+#include <samconf/samconf.h>
+
+#include "elos/config/config.h"
+#include "elos/pluginmanager/pluginmanager.h"
+#include "elos/scannermanager/scannermanager.h"
+
+typedef struct elosUnitTestState {
+    elosScannerManager_t scannermanager;
+    elosPluginManager_t pluginmanager;
+    samconfConfig_t *mockConfig;
+} elosUnitTestState_t;
+
+samconfConfigStatusE_t elosUtilCreateMockConfig(json_object *jobj, bool isSigned, samconfConfig_t *config);
+samconfConfigStatusE_t elosGetMockConfig(samconfConfig_t *config);
+int elosMockConfigCleanup(samconfConfig_t *config);
+
+TEST_CASE_FUNC_PROTOTYPES(elosTestElosScannerManagerStartSuccess)
+TEST_CASE_FUNC_PROTOTYPES(elosTestElosScannerManagerStartErrScannerManagerNull)
+TEST_CASE_FUNC_PROTOTYPES(elosTestElosScannerManagerStartExtErrPluginManagerLoad)
