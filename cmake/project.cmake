3e821049242153ff10f3298e018258d8d737a809
Wolfgang Gehrhardt <wolfgang.gehrhardt@emlix.com>
2024-07-18 17:15:26 +0200

    elos: Update to 0.60.11

--- cmake/project.cmake
+++ cmake/project.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-set(ELOS_VERSION 0.60.10)
+set(ELOS_VERSION 0.60.11)
 
 # Attention: Aside from the version, as many things as possible in this file
 #   should be put into functions, as this solves potential issues with commands
