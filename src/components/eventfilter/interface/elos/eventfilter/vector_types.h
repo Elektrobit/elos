eda8cd1a160fc79c625f4e9b8ef97e5923a0d452
Friedrich Schwedler <fschwedler@emlix.com>
2024-08-13 16:11:01 +0200

    Fix rpnfilter and eventfilter header for c++
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

--- src/components/eventfilter/interface/elos/eventfilter/vector_types.h
+++ src/components/eventfilter/interface/elos/eventfilter/vector_types.h
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 #pragma once
 
+#include <sys/cdefs.h>
 #include "elos/rpnfilter/vector_types.h"
 
+__BEGIN_DECLS
+
 typedef elosRpnFilterVector_t elosEventFilterVector_t;
+
+__END_DECLS
