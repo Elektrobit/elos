1ec838881a7fb49200f192f9ea03da9dd56a1f08
Friedrich Schwedler <fschwedler@emlix.com>
2024-09-05 10:33:10 +0200

    Extend mocking library for eventfilter
    - update mock for elosLogAggregatorFindEvents
    - add mock for elosEventfilterExecuteInTimeRange
    
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

--- test/utest/mocks/components/eventlogging/mock_LogAggregator.c
+++ test/utest/mocks/components/eventlogging/mock_LogAggregator.c
@@ -32,12 +32,14 @@ MOCK_FUNC_BODY(elosLogAggregatorShutdown, safuResultE_t, elosLogAggregator_t *lo
 }
 
 MOCK_FUNC_BODY(elosLogAggregatorFindEvents, safuResultE_t, elosLogAggregator_t *logAggregator, const char *rule,
-               safuVec_t *events) {
+               struct timespec const *newest, struct timespec const *oldest, safuVec_t *events) {
     if (MOCK_IS_ACTIVE(elosLogAggregatorFindEvents)) {
         check_expected_ptr(logAggregator);
         check_expected_ptr(rule);
+        check_expected_ptr(newest);
+        check_expected_ptr(oldest);
         check_expected_ptr(events);
         return mock_type(safuResultE_t);
     }
-    return MOCK_FUNC_REAL(elosLogAggregatorFindEvents)(logAggregator, rule, events);
+    return MOCK_FUNC_REAL(elosLogAggregatorFindEvents)(logAggregator, rule, newest, oldest, events);
 }
