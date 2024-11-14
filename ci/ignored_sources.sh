b1f79f4e1e380222467097f611827868e61b826b
Friedrich Schwedler <fschwedler@emlix.com>
2024-11-14 15:04:40 +0100

    Ignore libelos-lite tests for linting in ci
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

--- ci/ignored_sources.sh
+++ ci/ignored_sources.sh
@@ -2,7 +2,9 @@ export IGNORE_SOURCES=" \
   src/plugins/storagebackends/nosqlbackend/*/* \
   src/libelos_lite/private/libelos_lite.c \
   src/libelos_lite/public/elos/libelos_lite.h \
+  src/libelos_lite/public/elos/lite/event.h \
   src/demos/demo_eloslite.c \
+  test/utest/libelos_lite/eloslitePublish/* \
   "
 export UNUSED_SOURCES=" \
   src/components/processfilter/public/elos/processfilter/vector.h \
