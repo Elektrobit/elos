11797bc009538fa6645a1e7eca77d7cf465282f8
Friedrich Schwedler <fschwedler@emlix.com>
2024-11-19 16:05:50 +0100

    Ignore all of libelos-lite in the CI
    Signed-off-by: Friedrich Schwedler <fschwedler@emlix.com>

--- ci/ignored_sources.sh
+++ ci/ignored_sources.sh
@@ -1,6 +1,6 @@
 export IGNORE_SOURCES=" \
   src/plugins/storagebackends/nosqlbackend/*/* \
-  src/libelos_lite/private/libelos_lite.c \
+  src/libelos_lite/private/* \
   src/libelos_lite/public/elos/libelos_lite.h \
   src/libelos_lite/public/elos/lite/event.h \
   src/demos/demo_eloslite.c \
