2ff2ac902f60c4be3c2994270a135cfe9ec75347
Friedrich Schwedler <fschwedler@emlix.com>
2025-09-10 16:07:56 +0200

    doc: update documentation for the dlt hv scanner

--- src/plugins/scanners/dlt_hv/interface/dlt_hv/scanner.h
+++ src/plugins/scanners/dlt_hv/interface/dlt_hv/scanner.h
@@ -11,6 +11,26 @@
 
 #define ELOS_DLT_MESSAGE_CODE 2042
 
+/******************************
+ * The Scanner plugin instance
+ *
+ * Member:
+ *      shmemFd: The file descriptor for with the shared memory buffer
+ *      offsetAddress: The configured value from what offset the buffer should start
+ *      bufferSize: The configured size of the buffer
+ *      shmemFile: The file from where to get the shared memory buffer
+ *      shmemLogEntries: The number of log entries the shared memory buffer can hold
+ *      shmemData: The memory mapped dlt buffer
+ *      localBufferCopy: A local copy of the dlt shared memory buffer
+ *      idxRead: The read index for the dlt shared memory buffer
+ *      parserQueue: The ring buffer to queue log entries for parsing
+ *      shmemDataSize: The size of the shared memory buffer
+ *      publisher: The publisher to use for publishing events
+ *      scannerThread: The thread with the scanner that looks for new dlt entries
+ *      stopCmd: The evntfd to tell the plugin to stop
+ *      moreToRead: The eventfd for the scanner thread to tell that more events can be parsed and published
+ *      sleepInterval: The time to sleep between checks in the dlt memory buffer
+ ******************************/
 typedef struct elosDltScanner {
     int shmemFd;
     size_t offsetAddress;
