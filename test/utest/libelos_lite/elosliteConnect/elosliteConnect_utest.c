094307816ca5e40ce04179472b4a2810f519bab6
Björn Kirchner <bk@emlix.com>
2026-07-21 12:07:59 +0200

    utests: add unit test to verify new elosliteConnectUnix function

---
+++ test/utest/libelos_lite/elosliteConnect/elosliteConnect_utest.c
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+#include "elosliteConnect_utest.h"
+
+TEST_SUITE_FUNC_PROTOTYPES(connectUtest)
+
+int main() {
+    const struct CMUnitTest tests[] = {
+        TEST_CASE(elosliteConnectUnix_test_success),
+    };
+
+    return RUN_TEST_SUITE(tests, connectUtest);
+}
+
+static int connectUtestSetup(UNUSED void **state) {
+    return 0;
+}
+
+static int connectUtestTeardown(UNUSED void **state) {
+    return 0;
+}
---
+++ test/utest/libelos_lite/elosliteConnect/elosliteConnect_utest.h
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+#include <cmocka_extensions/cmocka_extensions.h>
+
+void elosliteConnectUnix_test_success(UNUSED void **state);
+
+TEST_CASE_FUNC_PROTOTYPES(elosliteConnectUnix_test_success)
