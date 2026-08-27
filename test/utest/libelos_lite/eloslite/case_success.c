05bfe4f1ed4c9ef2af476dab2617fd94bc1d4893
Björn Kirchner <bk@emlix.com>
2026-08-27 13:34:20 +0200

    feat: add printf-like convinience function for message logging

---
+++ test/utest/libelos_lite/eloslite/case_success.c
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+#include <elos/event/event.h>
+#include <elos/libelos_lite.h>
+#include <elos/lite/event.h>
+#include <string.h>
+
+#include "eloslite_utest.h"
+
+static const elosSeverityE_t severity = ELOS_SEVERITY_INFO;
+static const char *compareString = "This is a test. Param1: AA, Param2: 1234";
+
+bool __wrap_eloslitePublish(elosliteSession_t *session, elosliteEvent_t *event) {
+    assert_non_null(session);
+    assert_non_null(event);
+
+    assert_int_equal(event->severity, severity);
+    assert_string_equal(event->payload, compareString);
+
+    return true;
+}
+
+void elosliteTestElosliteLogMessageFormatedSuccess(UNUSED void **state) {
+    elosliteSession_t session = {0x00};
+
+    assert_true(elosliteLogMessageFormated(&session, severity, "This is a test. Param1: %s, Param2: %d", "AA", 1234));
+}
+
+int elosliteTestElosliteLogMessageFormatedSuccessSetup(UNUSED void **state) {
+    return 0;
+}
+
+int elosliteTestElosliteLogMessageFormatedSuccessTeardown(UNUSED void **state) {
+    return 0;
+}
